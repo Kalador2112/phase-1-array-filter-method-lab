7a9202e6ac8a52e10663495cf88d932ed82e875a
Kadin Caldwell <kcwell000@gmail.com>
2023-02-22 16:47:10 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    const name = array.filter (word => word.toUpperCase() === string.toUpperCase())
+    return name
+}
+
+
+
+function fuzzyMatch(array, string) {
+    const letters = array.filter (word => word.slice(0,2) === string.slice(0,2))
+    return letters
+}
+
+
+
+function matchName(array, string) {
+    const drivers = array.filter (({name}) => name === string)
+    return drivers
+}
